bceb099aac60f84a0daf781d7a4c33faf99071b2
[Alvaro De Vasconcelos] <[adevas01@mail.bbk.ac.uk]>
2023-10-24 13:39:25 +0100

    First Commit

---
+++ DoorTester.java
@@ -0,0 +1,10 @@
+public class DoorTester {
+  public static void main(String[] args) {
+
+    Door door = new Door("front door", "open");
+    System.out.println(door.getName());
+    System.out.println(door.getState());
+
+  }
+  
+}
