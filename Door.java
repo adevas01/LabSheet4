455ec3283d167d5dcd7a76a3bf4c7d6af4c4974c
[Alvaro De Vasconcelos] <[adevas01@mail.bbk.ac.uk]>
2023-10-25 13:06:34 +0100

    Modified

--- Door.java
+++ Door.java
@@ -1,26 +1,37 @@
+/*
+ * 1) Create class Door
+*/
 public class Door {
-
+  // 2) Instance variables
   private String name;
-  private String state;
+  private String state;    
 
+  // Constructor
   public Door(String name, String state) {
     this.name = name;
-    this.state = state;
+    this.state = state;    
   } 
-
+  // 6) Setters for name and state
   public void setName(String newName) {
     this.name = newName;
   }
-
   public void setState(String newState) {
     this.state = newState;
   }
-  
-  public String getName() {
-    return name;
+  // 3
+  public void close() {
+    System.out.println("The door is closed now"); 
+  }
+  public void open() {
+    System.out.println("The door is openned now");
   }
 
+
+  // Part 5 getters
   public String getState() {
     return state;
-  }
+  }   
+  public String getName() {
+    return name;
+  } 
 }
--- DoorTester.java
+++ DoorTester.java
@@ -2,18 +2,40 @@
 public class DoorTester {
   public static void main(String[] args) {  
 
+    // 4) Door object 
+    Door frontDoor = new Door("Front", "open");
+    System.out.println("The front door is " + frontDoor.getState());
+    System.out.println("Expected: open");
+    System.out.println("........................................");
 
-    Door door1 = new Door("front door", "open");
+    Door backDoor = new Door("Back", "closed");
+    System.out.println("The back door is " + backDoor.getState());
+    System.out.println("Expected: closed");
+    System.out.println(".........................................");
 
-    door1.setName("back door");
-    door1.setState("closed");
-    System.out.println("The " + door1.getName() + " is " + door1.getState());
+    // Use mutators/setters to change the state variable
+    backDoor.setState("open");
+    System.out.println("The back door is " + backDoor.getState());
+    System.out.println("Expected: open");
+    System.out.println("..........................................");  
 
-    Door door2 = new Door("side door", "closed");
+    // Use mutators/setters to change the name variable
+    backDoor.setName("Kitchen");
+    System.out.println("The back door is called " + backDoor.getName());
+    System.out.println("Expected: Kitchen");
+    System.out.println(".........................................."); 
 
-    door2.setName("front door");
-    door2.setState("open");
-    System.out.println("The " + door2.getName() + " is " + door2.getState());
+    // door object sideDoor
+    Door sideDoor = new Door("Side", "closed");
+    System.out.println("The side door is " + sideDoor.getState());
+    System.out.println("Expected: closed");
+    sideDoor.setState("open");
+    System.out.println("The side door is " + sideDoor.getState());
+    System.out.println("Expected: open");
+    System.out.println(".........................................."); 
+    
+
+    
 
 
 
